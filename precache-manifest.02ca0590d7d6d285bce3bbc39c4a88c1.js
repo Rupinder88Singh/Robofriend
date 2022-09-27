759a36c6698b03be078c14d4a58d99d6c1384939
Singh <rupinder.singh@nokia.com>
2022-09-27 21:56:02 +0530

    Updates

---
+++ precache-manifest.02ca0590d7d6d285bce3bbc39c4a88c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9fb167676c97acd242f9",
+    "url": "/Robofriend/static/css/main.dbf142b6.chunk.css"
+  },
+  {
+    "revision": "9fb167676c97acd242f9",
+    "url": "/Robofriend/static/js/main.9fb16767.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Robofriend/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "1053f2ff29e2224be64a",
+    "url": "/Robofriend/static/css/2.34dd78b5.chunk.css"
+  },
+  {
+    "revision": "1053f2ff29e2224be64a",
+    "url": "/Robofriend/static/js/2.1053f2ff.chunk.js"
+  },
+  {
+    "revision": "d1ad2ecb7b41926847fc",
+    "url": "/Robofriend/static/js/runtime~main.d1ad2ecb.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robofriend/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "ae0c13c68de0e37e22ea80655749b0ac",
+    "url": "/Robofriend/index.html"
+  }
+];
